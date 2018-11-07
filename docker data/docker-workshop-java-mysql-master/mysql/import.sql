0b2dcd7d47af136deaffa06762293c8da4079ef2
kentaza <41359596+kentaza@users.noreply.github.com>
2018-11-07 09:20:59 +0700

    Add files via upload

---
+++ docker data/docker-workshop-java-mysql-master/mysql/import.sql
@@ -0,0 +1,11 @@
+USE sample;
+
+CREATE TABLE USER (
+  id INT(11),
+  name char(60)
+) ENGINE=INNODB;
+
+INSERT INTO USER VALUES(1, 'Sample name 01');
+INSERT INTO USER VALUES(2, 'Sample name 02');
+INSERT INTO USER VALUES(3, 'Sample name 03');
+INSERT INTO USER VALUES(4, 'Sample name 04');
